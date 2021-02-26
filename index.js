c9238e16b1041cbb5eb52d9cf5c6477a34d52a22
shivakrishna51 <shivakrishna@paccore.com>
2021-02-26 16:35:55 +0530

    Changes the Hello world1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello World");
+ return res.send("Hello World i am from github");
 });
 
 // listen to port 7000 by default
